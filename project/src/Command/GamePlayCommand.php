80e7e22b3437a2929926d41c560d91d3deea772d
Michal Jenis <jenis.michal@gmail.com>
2025-04-19 09:46:09 +0200

    Load input XML file

--- project/src/Command/GamePlayCommand.php
+++ project/src/Command/GamePlayCommand.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\File\FileNotExistException;
+use App\File\FileNotReadableException;
+use App\File\Loader;
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,6 +17,15 @@ final class GamePlayCommand extends Command
 {
     private const INPUT_ARG = 'i';
     private const OUTPUT_ARG = 'o';
+
+    private Loader $fileLoader;
+
+    public function __construct(
+        Loader $fileLoader
+    ) {
+        parent::__construct();
+        $this->fileLoader = $fileLoader;
+    }
 
     protected function configure(): void
     {
@@ -25,6 +38,41 @@ final class GamePlayCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        try {
+            $inputXml = $this->getInputXml($input);
+        } catch (InvalidArgumentException $e) {
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            return -1;
+        }
+
+        try {
+            $content = $this->fileLoader->load($inputXml);
+        } catch (FileNotExistException | FileNotReadableException $e) {
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            return -1;
+        }
+
         return 0;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    private function getInputXml(InputInterface $input): string
+    {
+        $inputXml = $input->getArgument(self::INPUT_ARG);
+
+        if (is_string($inputXml) === false) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid argument provided for input XML. Expected a string, but received %s.', gettype($inputXml))
+            );
+        }
+
+        if (trim($inputXml) === '') {
+            throw new InvalidArgumentException('Input XML argument cannot be an empty string.');
+        }
+
+        return $inputXml;
     }
 }
